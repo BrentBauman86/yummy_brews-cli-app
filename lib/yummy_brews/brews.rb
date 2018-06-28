4b0f86e50095041fe83461ad6e26e8a200341d74
Brent Bauman <brentbauman86@gmail.com>
2018-06-28 21:33:46 +0000

    work

--- lib/yummy_brews/brews.rb
+++ lib/yummy_brews/brews.rb
@@ -1,42 +1,36 @@
 class YummyBrews::Brews
 
-  attr_accessor :name, :brewery, :price_range, :abv
+  attr_accessor :name, :brewery, :abv
 
   def self.brews
     wheat_beers = self.new
     wheat_beers.name = "Blue Moon"
     wheat_beers.brewery = "Blue Moon Brewing Company"
-    wheat_beers.price_range = "medium"
     wheat_beers.abv = "5.4%"
 
     ipa_beers = self.new
     ipa_beers.name = "Blue Moon"
     ipa_beers.brewery = "Blue Moon Brewing Company"
-    ipa_beers.price_range = "medium"
     ipa_beers.abv = "5.4%"
 
     stout_beers = self.new
     stout_beers.name = "Blue Moon"
     stout_beers.brewery = "Blue Moon Brewing Company"
-    stout_beers.price_range = "medium"
     stout_beers.abv = "5.4%"
 
     hefeweizen_beers = self.new
     hefeweizen_beers.name = "Blue Moon"
     hefeweizen_beers.brewery = "Blue Moon Brewing Company"
-    hefeweizen_beers.price_range = "medium"
     hefeweizen_beers.abv = "5.4%"
 
     pilsner_beers = self.new
     pilsner_beers.name = "Blue Moon"
     pilsner_beers.brewery = "Blue Moon Brewing Company"
-    pilsner_beers.price_range = "medium"
     pilsner_beers.abv = "5.4%"
 
     ale_beers = self.new
     ale_beers.name = "Blue Moon"
     ale_beers.brewery = "Blue Moon Brewing Company"
-    ale_beers.price_range = "medium"
     ale_beers.abv = "5.4%"
 
     [wheat_beers, ipa_beers, stout_beers, hefeweizen_beers, pilsner_beers, ale_beers]
@@ -44,11 +38,9 @@ class YummyBrews::Brews
 
   def brew_scraper
     # brews = []
-    # doc = Nokogiri::HTML(open(https://untappd.com))
-    # binding.pry
+    doc = Nokogiri::HTML(open(https://untappd.com))
+    binding.pry
     # this needs to return our array of brews!!
-    # brews
-    # more brews
   end
 
 end
