9bed5df0989f17fdd49cb63774864f80cc517912
Brent Bauman <brentbauman86@gmail.com>
2018-06-29 23:29:42 +0000

    more updates to CLI

--- lib/yummy_brews/cli.rb
+++ lib/yummy_brews/cli.rb
@@ -29,22 +29,28 @@ class YummyBrews::CLI
   def start
     input = nil
     puts ""
-    puts "Enter the brew number which fits your fancy:"
+    puts "Enter the brew number which fits your fancy, if nothing appeals to you please type exit."
     input = gets.strip
 
     if input == "1"
       puts "Yummy Wheat beers here"
+      start
     elsif input == "2"
       puts "ipas"
+      start
     elsif input == "3"
       puts "stout"
+      start
     elsif input == "4"
       puts "hefe"
+      start
     elsif input == "5"
       puts "pilsner"
+      start
     elsif input == "6"
       puts "ale"
-    else
+      start
+    elsif input == "exit"
       puts "Goodbye, hope that made you thirsty!"
     end
 end
