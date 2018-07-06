ab24373214640c920ef518a8026fe87f9a54c382
Brent Bauman <brentbauman86@gmail.com>
2018-07-06 23:21:34 +0000

    added second layer details

--- lib/yummy_brews/brew.rb
+++ lib/yummy_brews/brew.rb
@@ -2,10 +2,16 @@ class Brew
 
   attr_accessor :name, :brewery, :abv
 
+  @@all = []
+
   def initialize(brew_hash)
     @name = brew_hash[:name]
     @brewery = brew_hash[:brewery]
     @abv = brew_hash[:abv]
   end
+
+def brew_details(details_hash)
+  @details = details_hash[:details]
+end
 
 end
--- lib/yummy_brews/brew_scraper.rb
+++ lib/yummy_brews/brew_scraper.rb
@@ -1,41 +1,5 @@
 class BrewScraper
 
-  attr_accessor :name, :brewery, :abv
-
-
-  #   wheat_beers = self.new
-  #   wheat_beers.name = "Blue Moon"
-  #   wheat_beers.brewery = "Blue Moon Brewing Company"
-  #   wheat_beers.abv = "5.4%"
-  #
-  #   ipa_beers = self.new
-  #   ipa_beers.name = "Blue Moon"
-  #   ipa_beers.brewery = "Blue Moon Brewing Company"
-  #   ipa_beers.abv = "5.4%"
-  #
-  #   stout_beers = self.new
-  #   stout_beers.name = "Blue Moon"
-  #   stout_beers.brewery = "Blue Moon Brewing Company"
-  #   stout_beers.abv = "5.4%"
-  #
-  #   hefeweizen_beers = self.new
-  #   hefeweizen_beers.name = "Blue Moon"
-  #   hefeweizen_beers.brewery = "Blue Moon Brewing Company"
-  #   hefeweizen_beers.abv = "5.4%"
-  #
-  #   pilsner_beers = self.new
-  #   pilsner_beers.name = "Blue Moon"
-  #   pilsner_beers.brewery = "Blue Moon Brewing Company"
-  #   pilsner_beers.abv = "5.4%"
-  #
-  #   ale_beers = self.new
-  #   ale_beers.name = "Blue Moon"
-  #   ale_beers.brewery = "Blue Moon Brewing Company"
-  #   ale_beers.abv = "5.4%"
-  #
-  #   # [wheat_beers, ipa_beers, stout_beers, hefeweizen_beers, pilsner_beers, ale_beers]
-  # end
-
   def self.brew_scraper
     doc = Nokogiri::HTML(open("https://untappd.com/search?q=beer"))
     brews = []
@@ -47,6 +11,13 @@ class BrewScraper
       brews << {:name => name, :brewery => brewery, :abv => abv}
         end
       end
-    end
-  brews
+    brews
+  end
+
+  def self.brew_scraper_details
+    doc = Nokogiri::HTML(open("https://untappd.com"))
+
+    details = doc.css("div.beer-description-read-less")
+
+  end
 end
