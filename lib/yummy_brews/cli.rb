55ffd934b8364d3f2423848423ffd8a6d9f6e8e1
Brent Bauman <brentbauman86@gmail.com>
2018-07-07 14:23:16 +0000

    start making user input logic

--- lib/yummy_brews/cli.rb
+++ lib/yummy_brews/cli.rb
@@ -8,8 +8,13 @@ class YummyBrews::CLI
   end
 
   def list_brews
-    Brew.all.each.with_index(1) do |brews, index|
-      puts "#{index}". "#{brews}"
+    Brew.all.each.with_index(1) do |brew, index|
+      puts "#{index}". "#{brew.name}"
+    end
+  end
+
+  def display_brew(integer)
+
   end
 
   def start
@@ -18,24 +23,9 @@ class YummyBrews::CLI
     puts "Enter the brew number which fits your fancy, if nothing appeals to you please type exit."
     input = gets.strip
 
-    if input == "1"
-      puts "Yummy Wheat beers here"
-      start
-    elsif input == "2"
-      puts "ipas"
-      start
-    elsif input == "3"
-      puts "stout"
-      start
-    elsif input == "4"
-      puts "hefe"
-      start
-    elsif input == "5"
-      puts "pilsner"
-      start
-    elsif input == "6"
-      puts "ale"
-      start
+    if input.to_i > 0 && input.to_i < Brew.all.length
+
+
     elsif input == "exit"
       puts "Goodbye, hope that made you thirsty!"
     end
