b33063a72aa4867f7060bd9c54e43382ae572355
Brent Bauman <brentbauman86@gmail.com>
2018-07-07 13:57:04 +0000

    added additional instance variables

--- lib/yummy_brews/brew.rb
+++ lib/yummy_brews/brew.rb
@@ -1,6 +1,6 @@
 class Brew
 
-  attr_accessor :name, :brewery, :abv
+  attr_accessor :name, :brewery, :abv, :brew_type, :rating, :brew_url
 
   @@all = []
 
@@ -8,6 +8,10 @@ class Brew
     @name = brew_hash[:name]
     @brewery = brew_hash[:brewery]
     @abv = brew_hash[:abv]
+    @brew_type = brew_hash[:brew_type]
+    @rating = brew_hash[:rating]
+    @brew_url = brew_hash[:brew_url]
+    @@all << self
   end
 
 def brew_details(details_hash)
